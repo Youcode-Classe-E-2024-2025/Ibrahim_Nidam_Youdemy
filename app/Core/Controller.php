fc682e242f22fa85c9da4270ea776eed1eaf2c0b
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-01-13 15:41:14 +0100

    added utility methods for the main controller

--- app/Core/Controller.php
+++ app/Core/Controller.php
@@ -1,3 +1,38 @@
 <?php
 
     namespace Core;
+
+    class Controller {
+
+        protected function showView($view, $data = []){
+            extract($data);
+
+            $viewPath = __DIR__ . "/../../app/Views/{$view}.php";
+            
+            if(file_exists($viewPath)){
+                require_once $viewPath;
+            } else {
+                die("View '{$view}' not found");
+            }
+        }
+
+        protected function redirect ($url){
+            header("Location: {$url}");
+            exit();
+        }
+
+        protected function setFlash($key, $message){
+            session_start();
+            $_SESSION["flash"][$key] = $message;
+        }
+
+        protected function getFlash($key){
+            session_start();
+            if(isset($_SESSION["flash"][$key])){
+                $message = $_SESSION["flash"][$key];
+                unset($_SESSION["flash"][$key]);
+                return $message;
+            }
+            return null;
+        }
+    }
