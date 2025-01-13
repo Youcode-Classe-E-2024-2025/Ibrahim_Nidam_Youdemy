378672aae5b63e3e355a77f387512bbb501adb2e
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-01-14 00:50:29 +0100

    small change in the sql script for users role to handle some cases in the future easily

--- Public/assets/data/script.sql
+++ Public/assets/data/script.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS users (
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role ENUM('visitor', 'student', 'teacher', 'admin') NOT NULL,
+    role ENUM('student', 'pending_teacher', 'teacher', 'admin') NOT NULL DEFAULT 'student',
     is_active BOOLEAN DEFAULT TRUE
 );
 
