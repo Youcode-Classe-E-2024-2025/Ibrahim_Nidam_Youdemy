a64aeb0f9bd3f7684deeaa151c8ff21ddf44aba1
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-01-13 15:57:05 +0100

    added a csrf middelware to handle if token exists or is it correct

--- app/Middleware/csrfMiddleware.php
+++ app/Middleware/csrfMiddleware.php
@@ -1,3 +1,26 @@
 <?php
 
     namespace Middleware;
+
+    use Security\Security;
+
+    class CsrfMiddleware {
+        private $security;
+
+        public function __construct(){
+            $this->security = new Security();
+        }
+
+        public function handle($request){
+
+            if($_SERVER["REQUEST_METHOD"] === "POST"){
+                $csrfToken = $request["csrf_token"] ?? null;
+
+                if(!$csrfToken || !$this->security->verifyCsrfToken($csrfToken)){
+                    http_response_code(403);
+                    die("Forbidden: Invalid CSRF Token.");
+                }
+            }
+            return true;
+        }
+    }
