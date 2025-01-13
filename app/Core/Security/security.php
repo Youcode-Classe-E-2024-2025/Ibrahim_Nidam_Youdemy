fbd478215a77464f5dc11139ddea16255811ce9f
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-01-13 15:52:01 +0100

    added an authentication middleware to redirect to login page

---
+++ app/Core/Security/security.php
@@ -0,0 +1,7 @@
+<?php
+
+    namespace Security;
+
+    class Security {
+        
+    }
--- app/Middleware/AuthMiddleware.php
+++ app/Middleware/AuthMiddleware.php
@@ -1,3 +1,17 @@
 <?php
 
     namespace Middleware;
+
+    class AuthMiddleware {
+
+        public function handle(){
+            session_start();
+
+            if(!isset($_SESSION["user"])){
+                header("Location: /login");
+                exit();
+            }
+
+            return true;
+        }
+    }
