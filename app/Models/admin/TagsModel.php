3119af814fdb631aabe08403b059874811655c82
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-01-17 23:47:13 +0100

    CLASS: added the tags class it can handle all crud and no duplicates allowed

--- app/Models/admin/TagsModel.php
+++ app/Models/admin/TagsModel.php
@@ -4,3 +4,49 @@ namespace AdminModel;
 
 use Core\Model;
 
+class TagsModel extends Model {
+
+    public function getAllTags(){
+        return $this->read("tags");
+    }
+
+    public function addTag($name){
+        $existingTag = $this->read("tags", ["name" => $name]);
+
+        if(!empty($existingTag)){
+            return false;
+        }
+
+        return $this->create("tags", ["name" => $name]);
+    }
+
+    public function addTags($tags){
+        $data = [];
+
+        foreach($tags as $tag){
+            $existingTag = $this->read("tags", ["name" => $tag]);
+            if(empty($existingTag)){
+                $data[] = ["name" => $tag];
+            }
+        }
+        if(!empty($data)){
+            return $this->create("tags", $data);
+        }
+
+        return false;
+    }
+
+    public function updateTag($id, $name){
+        $existingTag = $this->read("tags", ["name" => $name]);
+
+        if(!empty($existingTag)){
+            return false;
+        }
+        
+        return $this->update("tags", ["name" => $name], ["id" => $id]);
+    }
+
+    public function deleteTag($id){
+        return $this->delete("tags", ["id" => $id]);
+    }
+}
