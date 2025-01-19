86b5116c2a43fa9b45a1ff550134ffd859c22c49
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-01-19 13:09:16 +0100

    fixed the teacher stats display

--- app/Controllers/users/TeacherController.php
+++ app/Controllers/users/TeacherController.php
@@ -3,7 +3,19 @@ namespace UsersController;
 
 class TeacherController extends UserController {
 
-    public function teacherDash(){
-        $this->showView("users/TeacherDash");
+    public function teacherDash() {
+        $teacherId = $_SESSION['id'] ?? null;
+
+        $total_courses = $this->stats->getTotalCoursesByTeacher($teacherId);
+        $total_students = $this->stats->getTotalStudentsByTeacher($teacherId);
+        $pending_courses = $this->stats->getPendingCoursesByTeacher($teacherId);
+
+        $data = [
+            'total_courses' => $total_courses,
+            'total_students' => $total_students,
+            'pending_courses' => $pending_courses,
+        ];
+
+        $this->showView("users/TeacherDash", $data);
     }
 }
